b8ecfee024349d00e3f5b4fffef0f12fd2194c01
Chris Storey <chrisearlybird@hotmail.com>
2024-10-14 01:16:57 +0100

    Sroted an error regarding images not displaying on index.ejs. for image upload i have some CRUD functionality left to implement , mainly deleting on front end and back end

--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,19 @@ const seedDB = async () => {
 			author: "66ede89a8297ff29de5415f8",
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)}, ${sample(places)}`,
-			image: `https://loremflickr.com/450/300/camping?random=${i}`,
 			description:
 				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 			price: pricing,
+			images: [
+				{
+					url: "https://res.cloudinary.com/dakwwvfiu/image/upload/v1728856546/YelpCamp/ghmxlmxpwsxswjn7efdb.jpg",
+					filename: "YelpCamp/ghmxlmxpwsxswjn7efdb",
+				},
+				{
+					url: "https://res.cloudinary.com/dakwwvfiu/image/upload/v1728856547/YelpCamp/gvlx9nho1tpv1fwbmtju.jpg",
+					filename: "YelpCamp/gvlx9nho1tpv1fwbmtju",
+				},
+			],
 		});
 		await camp.save();
 	}
